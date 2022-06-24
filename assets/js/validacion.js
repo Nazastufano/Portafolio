7b369cd7695a9837ab789c20c0a550e4903f7dc4
Nazareno <stufanonaza@hotmail.com>
2022-06-23 21:48:42 -0300

    Agregando visuales a la página y validaciones al formulario

--- assets/js/validacion.js
+++ assets/js/validacion.js
@@ -7,10 +7,10 @@ export function valida(input){
     }
 
     if(input.validity.valid){
-        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.classList.remove("formcontato__input-incorrecto");
         input.parentElement.querySelector(".input-message-error").innerHTML = "";
     }else{
-        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.classList.add("formcontato__input-incorrecto");
         input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
     }
 }
@@ -44,3 +44,54 @@ function mostrarMensajeDeError(tipoDeInput, input){
     return mensaje
 }
 */
+
+const formulario = document.getElementById("formulario");
+const inputs = document.querySelectorAll("#formulario input");
+
+const expresiones = {
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,50}$/,
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+}
+
+const campos = {
+    nombre: false,
+    email: false,
+    asunto: false
+}
+
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "nombre":
+            validarCampo(expresiones.nombre, e.target, "nombre");
+        break;
+        case "email":
+            validarCampo(expresiones.email, e.target, "email");
+        break;
+        case "asunto":
+            validarCampo(expresiones.nombre, e.target, "asunto");
+        break;
+    }
+}
+
+const validarCampo = (expresion, input, campo) =>{
+    if(expresion.test(input.value)){
+        document.getElementById(`formcontacto__${campo}`).classList.remove("formcontato__input-incorrecto");
+        campos[campo] = true;
+    } else { 
+        document.getElementById(`formcontacto__${campo}`).classList.add("formcontato__input-incorrecto");
+        campos[campo] = false;
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", validarFormulario);
+    input.addEventListener("blur", validarFormulario);
+});
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if(campos.nombre && campos.email && campos.asunto){
+        formulario.reset();
+    }
+});
